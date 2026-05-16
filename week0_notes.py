5066d6a3833936e0242f9582c1927f25d04d799a
lochan <lochan1410a@gmail.com>
2026-05-17 01:28:47 +0530

    completed full week 0 lecture

--- week0_notes.py
+++ week0_notes.py
@@ -1,8 +1,16 @@
  # CS50P Harvard Course
 #week0_notes - introduction to python 
 
-#day 1-learned functions,variables,parameters,argumens,return values,side effects, bugs ,
+#day 1-learned functions,variables,parameters,argumens
+# ,return values,side effects, bugs 
 
 print("hello world")
 print("my name is lochan")
 print("i am learning python")
+
+
+""" day 1 time[1:22 am]  - completed week 0  lecture 
+topics covered-functions,variables,parameters,arguments
+,return valus,sting,side effect,bugs,comments,print(),
+operators,int,float,scope,interactive mode,def,
+pseudocode"""
