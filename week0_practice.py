437b1fd04f3302eb7dd7a5ca4bc49954dd5256d9
lochan <lochan1410a@gmail.com>
2026-05-16 21:39:16 +0530

    week0 - day 1 practice

---
+++ week0_practice.py
@@ -0,0 +1,10 @@
+#week0   day 1  practice 
+
+# asking user for name 
+n=input("what is your name? ")
+print("my name is", n) 
+ 
+
+#greeting user
+x=input("what is your name? ")
+print("hello",  x)
