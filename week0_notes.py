90d46bc43fa35ab7f8f5ec14d14efe1d25a5d95e
lochan <lochan1410a@gmail.com>
2026-05-16 21:27:41 +0530

    week 0 - day 1 functions, arguments, parameters, print, return, side effects

--- week0_notes.py
+++ week0_notes.py
@@ -1,6 +1,8 @@
  # CS50P Harvard Course
-#week0_notes - funtions and variables 
+#week0_notes - introduction to python 
 
+#day 1-learned functions,variables,parameters,argumens,return values,side effects, bugs ,
 
-print("hello, world")
+print("hello world")
 print("my name is lochan")
+print("i am learning python")
