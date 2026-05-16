11cc8cfad1c63dfc8eec4b8ee52c02ea607962ec
lochan <lochan1410a@gmail.com>
2026-05-17 01:21:19 +0530

    week 0 all 5 problems finished

---
+++ week0_problems.py
@@ -0,0 +1,42 @@
+#week 0 - problem sets
+
+
+#problem 1 - indoor voice
+a=input("enter the str=") 
+print(a.lower())
+  
+
+
+# problem 2- playback speed
+x=input("enter the sentence=")
+print(x.replace(" ", "..."))
+
+
+
+
+#problem 3- making faces
+n=input("enter the str=")
+n=n.replace(":)", "🙂")
+n=n.replace(":(", "😞")
+print(n)
+
+
+
+
+#problem 4-einstein
+m=int(input("enter the mass="))
+c=300000000
+e=m*c**2
+print(e)
+
+
+
+
+
+
+#problem 5- tip calculator
+x=float(input("enter the meal  cost="))
+y=int(input("enter the tip="))
+t=x*(y/100)
+print(f"tip ${t}")
+
