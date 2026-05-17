cae0bbbf1323c39cccaf41f8140724b22ae58a77
lochan <lochan1410a@gmail.com>
2026-05-17 10:39:45 +0530

    code for asking age

--- week0_practice.py
+++ week0_practice.py
@@ -8,3 +8,10 @@ print("my name is", n)
 #greeting user
 x=input("what is your name? ")
 print("hello",  x)
+
+
+#asking for age 
+age=input("what is your age?")
+print(f"you are {age}  years old")
+
+
