cfc67952fde61f556f2c485fc6ffa60cb465cb57
lochan <lochan1410a@gmail.com>
2026-05-17 20:50:35 +0530

    updated all built in functions and string methods

--- week0_notes.py
+++ week0_notes.py
@@ -14,3 +14,27 @@ topics covered-functions,variables,parameters,arguments
 ,return valus,sting,side effect,bugs,comments,print(),
 operators,int,float,scope,interactive mode,def,
 pseudocode"""
+
+"""day 2 full revision of day 1 topics of built in functions 
+
+all built in string methods:
+.lower()        → "HELLO" → "hello"
+.upper()        → "hello" → "HELLO"
+.strip()        → "  hi  " → "hi"
+.replace(a,b)   → replaces a with b
+.split()        → "hi bye" → ["hi","bye"]
+.title()        → "hi man" → "Hi Man"
+.capitalize()   → "hi man" → "Hi man"
+.count()        → counts how many times
+.find()         → finds position of character                 
+
+
+all built in functions:
+print()      → shows output on screen
+input()      → takes user input
+int()        → converts to integer
+float()      → converts to decimal
+str()        → converts to string
+len()        → counts length
+round()      → rounds decimal numbers
+type()       → shows what type it is                   """
