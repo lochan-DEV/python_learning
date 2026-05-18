586eb596c164e3a4005c8d2ebd5eb20d26e184cc
lochan <lochan1410a@gmail.com>
2026-05-18 22:31:44 +0530

    solved 2nd problem

--- week1_problems.py
+++ week1_problems.py
@@ -1,4 +1,4 @@
-#problem 1 deep thought
+#problem 1- deep thought
 x=input("what is the answer? ").lower().strip()
 if x=="42":
     print("yes")
@@ -8,3 +8,13 @@ elif x=="forty-two":
     print("yes")
 else:
     print("no")
+
+
+#problem 2- bank 
+x=input("enter the sentence:").lower().strip()
+if x.startswith("hello"):
+    print("$0")
+elif x.startswith("h"):
+    print("$20")
+else:
+    print("$100")
