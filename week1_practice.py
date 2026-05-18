63acd256e7b2df99f8e30af11e5a679ade32a487
lochan <lochan1410a@gmail.com>
2026-05-18 22:46:20 +0530

    practicing using the concepts of week 1 lec

---
+++ week1_practice.py
@@ -0,0 +1,12 @@
+#practice problem 1:
+x=int(input("enter the marks:"))
+if 90<= x <=100:
+    print("A grade")
+elif 80<= x <90:
+    print("B grade")
+elif 70<=x<80:
+    print("C grade")
+elif 60<=x<70:
+    print("D grade")
+else:
+    print("fail")
