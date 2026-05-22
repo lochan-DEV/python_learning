8eeeec2323217058997e2e3f247c159c56a2399a
lochan <lochan1410a@gmail.com>
2026-05-22 23:04:33 +0530

    day 1 of learning week 3

---
+++ week3_notes.py
@@ -0,0 +1,38 @@
+# week 3 - exceptions
+
+# day 1 of learning week 3 lecture
+
+
+""" learnt about exception,value error,name error,try , except ,else  and revised the 
+concepts of week 0 to week 2 with some practice of decoding my own problems 
+
+# syntax error -  "it  occurs when the rules (syntax) of a programming language are 
+                 not followed correctly 
+    [in simple  words breaking the grammer rules of the python language]
+    eg: print("hello world"    [# missing closing parenthesis]
+    
+ 
+# exception - "it is an error that occurs during the execution of a program                                                                                                         
+ 
+
+# value error - "it occurs when a function receives an argument of the correct type
+                 but an inappropriate value" 
+
+
+# name error - "it occurs when a variable is not defined or is not accessible in the
+                current scope"
+ 
+
+# try - "it is used to wrap code that may raise an exception"                
+                 
+                  
+# except - "it is used to handle exceptions that may occur in the try block "                  
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                                       """
+            
