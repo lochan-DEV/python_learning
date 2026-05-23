73fe71a2ec7a39bd3774164a199854b8eb29c052
lochan <lochan1410a@gmail.com>
2026-05-23 23:47:49 +0530

    asking user age by handling the value error

--- week3_practice.py
+++ week3_practice.py
@@ -21,3 +21,13 @@ def calculator():
 
 if __name__ == "__main__":
     calculator()
+
+
+
+#asking user for age with exception handling
+try:
+    age = int(input("Enter your age: "))
+    print(f"You are {age} years old.")
+
+except ValueError:
+    print("That's not a valid age!")
