169aed49bb681be7cbd1da4203c0a8ebba4ad870
lochan <lochan1410a@gmail.com>
2026-05-24 18:20:25 +0530

    solved 2nd problem

--- week3_problems.py
+++ week3_problems.py
@@ -27,3 +27,33 @@ def fuel():
 
 if __name__ == "__main__":
     main()
+
+
+
+# problem 2-
+food={"Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00}
+total=0
+while True:
+    try:
+        x=input("Item :")
+
+        if x.title() in food:
+            total=total+food[x.title()]
+            print(f"Total: ${total:.2f}")
+        else:
+            print("Item: ")
+
+    except ValueError:
+        pass
+    except EOFError:
+        break
+    except x not in food:
+        pass
