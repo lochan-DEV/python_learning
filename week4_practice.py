225964c3bb6b4e5bb2870b4b26adb796c13422ed
lochan <lochan1410a@gmail.com>
2026-05-26 23:41:59 +0530

    hello message using cowsay module

--- week4_practice.py
+++ week4_practice.py
@@ -3,6 +3,28 @@
 # to give random numbers as output
 
 import random
+
+import pip
 num = random.randint(1, 50)
 print(f"Random number: {num}")
 print("Even" if num % 2 == 0 else "Odd")
+
+#
+
+import cowsay
+
+animals = {
+    "cow": cowsay.cow,
+    "tux": cowsay.tux,
+    "dragon": cowsay.dragon,
+    "ghostbusters": cowsay.ghostbusters,
+    "kitty": cowsay.kitty,
+    "meow": cowsay.meow,
+    "stegosaurus": cowsay.stegosaurus}
+
+x = input("enter animal name: ").lower()
+
+if x in animals:
+    animals[x]("hello")
+else:
+    print("animal not found")
