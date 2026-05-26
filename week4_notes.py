fb45db2ffbf714fd172d16ad0766b0aaa98dd5c3
lochan <lochan1410a@gmail.com>
2026-05-26 23:08:24 +0530

    day 1 of learning Libraries

---
+++ week4_notes.py
@@ -0,0 +1,11 @@
+# day 1 of learning week 4 lecture 
+
+# What is a library / module?
+# A module is a file with pre-written code (functions)
+# that you can import and use in your own code
+# Instead of writing everything from scratch!
+                 
+#                   example:import random        # import whole module
+
+
+
