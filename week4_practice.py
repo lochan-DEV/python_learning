a61b2c8e0027382af0dabc39f3669982eea5e5d0
lochan <lochan1410a@gmail.com>
2026-05-26 23:15:42 +0530

    random number generation logic

---
+++ week4_practice.py
@@ -0,0 +1,8 @@
+# practicing using libraries 
+
+# to give random numbers as output
+
+import random
+num = random.randint(1, 50)
+print(f"Random number: {num}")
+print("Even" if num % 2 == 0 else "Odd")
