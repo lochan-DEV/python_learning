0da479551fce1c641892e5678946ef8386657969
lochan <lochan1410a@gmail.com>
2026-05-28 22:50:25 +0530

    solved 2nd problem

--- week4/problems.py
+++ week4/problems.py
@@ -4,3 +4,32 @@ import emoji
 x=input("enter the name of an emoji: ")
 result= emoji.emojize(x , language="alias")
 print(result)
+
+
+# problem 2 
+import sys
+import random
+from pyfiglet import Figlet
+
+if len(sys.argv)>3 or len(sys.argv)==2:
+    sys.exit("invalid")
+elif len(sys.argv)==1:
+    figlet=Figlet()
+    font=figlet.getFonts()
+    f = random.choice(font)
+    figlet.setFont(font=f)
+    x= input("Input: ")
+    print(figlet.renderText(x))
+
+
+elif len(sys.argv)==3:
+    figlet=Figlet()
+    if sys.argv[1]!='-f' and sys.argv[1]!='--font':
+        sys.exit("invalid")
+    elif sys.argv[2] not in figlet.getFonts():
+        sys.exit("invalid")
+    else:
+
+        figlet.setFont(font=sys.argv[2])
+        x=input("input :")
+        print(figlet.renderText(x))
