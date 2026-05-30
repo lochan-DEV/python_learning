bf38df620167dcef6644f5f8289e0fc7fdf86a07
lochan <lochan1410a@gmail.com>
2026-05-30 23:35:24 +0530

    solved 4th problem

--- week4/problems.py
+++ week4/problems.py
@@ -64,3 +64,53 @@ while True:
         print("Too large!")
     else:
         print("Too small!")
+
+
+
+
+# problem 4
+import random
+
+def main():
+    n = get_level()
+    score = 0
+    for i in range(10):
+        x = generate_integer(n)
+        y = generate_integer(n)
+        correct = False
+        for j in range(3):
+            try:
+                z = int(input(f"{x} + {y} = "))
+                if z == x + y:
+                    score += 1
+                    correct = True
+                    break
+                else:
+                    print("EEE")
+            except ValueError:
+                print("EEE")
+        if not correct:
+            print(f"{x} + {y} = {x + y}")
+    print(f"Score: {score}")
+
+def get_level():
+    while True:
+        try:
+            level = int(input("Level: "))
+            if level in [1, 2, 3]:
+                return level
+        except ValueError:
+            pass
+
+def generate_integer(level):
+    if level == 1:
+        return random.randint(0, 9)
+    elif level == 2:
+        return random.randint(10, 99)
+    elif level == 3:
+        return random.randint(100, 999)
+    else:
+        raise ValueError
+
+if __name__ == "__main__":
+    main()
