e45b2fa6b57c62d75a1561546f454834141cbb9d
lochan <lochan1410a@gmail.com>
2026-05-31 23:41:26 +0530

    solved 5th problem

--- week4/problems.py
+++ week4/problems.py
@@ -114,3 +114,29 @@ def generate_integer(level):
 
 if __name__ == "__main__":
     main()
+
+
+
+
+# problem 5
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit("Missing command-line argument")
+
+try:
+    n = float(sys.argv[1])
+except ValueError:
+    sys.exit("Command-line argument is not a number")
+
+try:
+    url = "https://rest.coincap.io/v3/assets/bitcoin?apiKey=YOUR_API_KEY"
+    response = requests.get(url)
+    data = response.json()
+    price = float(data["data"]["priceUsd"])
+except requests.RequestException:
+    sys.exit("Network error")
+
+total = n * price
+print(f"${total:,.4f}")
