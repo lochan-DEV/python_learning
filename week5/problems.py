a796c4104616772f9d188f5c57ef293a25a77bbf
lochan <lochan1410a@gmail.com>
2026-06-12 21:32:25 +0530

    solved 2nd problem

--- week5/problems.py
+++ week5/problems.py
@@ -70,6 +70,28 @@ if __name__ == "__main__":
 
 # to test the above function 
 
+from bank import value           # this code is used to test the above function
+
+def test_hello():
+    assert value("hello") == "$0"
+
+def test_hello_phrase():
+    assert value("hello there") == "$0"
+
+def test_case_insensitive():
+    assert value("HELLO") == "$0"
+
+def test_h_greeting():
+    assert value("hey") == "$20"
+
+def test_h_uppercase():
+    assert value("Hey!") == "$20"
+
+def test_other():
+    assert value("what's up") == "$100"
+
+def test_empty():
+    assert value("") == "$100"
 
 
 
