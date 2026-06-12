c8952348151e1caf1f741b43b46cd0e79a5c7064
lochan <lochan1410a@gmail.com>
2026-06-12 20:38:45 +0530

    solved 1st problem

--- week5/problems.py
+++ week5/problems.py
@@ -16,6 +16,33 @@ def shorten(text):
 
     return result
 
+if __name__ == "__main__":
+    main()
 
 # to test the above function
+
+from twttr import shorten        # this code is used to test the above function
+
+def test_lowercase():
+    assert shorten("twitter") == "twttr"
+
+def test_uppercase():
+    assert shorten("TWITTER") == "TWTTR"
+
+def test_mixed_case():
+    assert shorten("TwItTeR") == "TwtTR"
+
+def test_numbers():
+    assert shorten("123456") == "123456"
+
+def test_punctuation():
+    assert shorten("hello, world!") == "hll, wrld!"
+
+def test_empty():
+    assert shorten("") == ""
+
+
+#########################################################################################
+
+
 
