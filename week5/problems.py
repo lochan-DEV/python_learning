1c14d94100ed59eb183a996718a5bb35cd4871e6
lochan <lochan1410a@gmail.com>
2026-06-12 20:36:03 +0530

    solved 1st problem

--- week5/problems.py
+++ week5/problems.py
@@ -1,1 +1,21 @@
+# problem 1 - testing the shorten function
+    
+def main():                              
+    x = input("Input: ")                 
+    print("Output:", shorten(x))
+
+
+
+def shorten(text):
+    vowels = "aeiouAEIOU"
+    result = ""
+
+    for c in text:
+        if c not in vowels:
+            result += c
+
+    return result
+
+
+# to test the above function
 
