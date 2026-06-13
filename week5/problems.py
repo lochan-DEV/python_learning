6b5b0cdbc972c781bed17ff05eb6a2c5d95ff612
lochan <lochan1410a@gmail.com>
2026-06-13 22:47:01 +0530

    solved 3rd problem

--- week5/problems.py
+++ week5/problems.py
@@ -130,6 +130,21 @@ if __name__=="__main__":
 
 # to test the above function
 
+from plates import is_valid
+
+def test_is_valid():
+
+    assert is_valid("HELLO")==True
+    assert is_valid("HELLO, WORLD")==False
+    assert is_valid("CS50")==True
+    assert is_valid("GOODBYE")==False
+    assert is_valid("CS05")==False
+    assert is_valid("50")==False
+    assert is_valid("CS,.0")==False
+    assert is_valid("CS50P")==False
+    assert is_valid("AS ASD")==False
+    assert is_valid("CS#$SD0")==False
+
 
 
 
