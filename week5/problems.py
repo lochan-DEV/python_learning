61d7eb0ce7ba645d37ff38e9ebbdb20a1d2c1499
lochan <lochan1410a@gmail.com>
2026-06-13 22:46:17 +0530

    solved 3rd problem

--- week5/problems.py
+++ week5/problems.py
@@ -94,5 +94,44 @@ def test_empty():
     assert value("") == "$100"
 
 
+#############################################################################################
+
+
+
+# problem 3 - testing is_valid function
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(x):
+    if len(x)<2 or len(x)>6:
+        return False
+    if not x[0:2].isalpha():
+        return False
+    if not x.isalnum():
+        return False
+    for i in range(len(x)):
+        if x[i].isdigit():
+            if x[i]=="0":
+                 return False
+            if not x[i:].isdigit():
+                return False
+            break
+    return True
+
+
+if __name__=="__main__":
+    main()
+
+
+# to test the above function
+
+
+
+
 
 
