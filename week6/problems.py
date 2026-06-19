9c5b0bd9a653ea61f709068aaa07012787fe6176
lochan <lochan1410a@gmail.com>
2026-06-20 00:22:39 +0530

    solved 2nd problem

--- week6/problems.py
+++ week6/problems.py
@@ -38,4 +38,46 @@ def check_len():
 
 
 if __name__=="__main__":
-    main() 
+    main()
+
+##################################################################
+
+# problem - 2 
+
+import sys
+import csv
+from tabulate import tabulate
+
+
+def main():
+     is_valid()
+     content=read_file()
+     print(content)
+
+
+
+def is_valid():
+        if len(sys.argv)<2:
+            sys.exit("too few command line arguments")
+        elif len(sys.argv)>2:
+            sys.exit("too many command line arguments")
+        elif not sys.argv[1].endswith(".csv"):
+            sys.exit("Not a CSV file")
+        else:
+            try:
+                with open(sys.argv[1]) as f:
+                    pass
+            except FileNotFoundError:
+                sys.exit("File does not exist")
+
+def read_file():
+     table=[]
+     with open(sys.argv[1]) as file:
+        reader = csv.reader(file)
+        for row in reader:
+            table.append(row)
+        return tabulate(table, headers="firstrow", tablefmt="grid")
+
+if __name__=="__main__":
+     main()
+
