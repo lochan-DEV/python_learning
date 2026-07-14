bc9744f0b67369ff1b8fb9dce4e8a9ed85b8c6ac
lochan-DEV <lochan1410a@gmail.com>
2026-07-14 22:46:50 +0530

    solved 3rd problem

--- week6/problems.py
+++ week6/problems.py
@@ -81,3 +81,44 @@ def read_file():
 if __name__=="__main__":
      main()
 
+
+
+#########################################################################################################################################
+
+# problem - 3
+
+
+import csv
+import sys
+
+
+def main():
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    try:
+        with open(input_file, "r") as infile:
+            reader = csv.DictReader(infile)
+
+            with open(output_file, "w") as outfile:
+                writer = csv.DictWriter(outfile, fieldnames=["first", "last", "house"])
+                writer.writeheader()
+
+                for row in reader:
+                    last, first = row["name"].split(", ")
+                    writer.writerow({"first": first, "last": last, "house": row["house"]})
+
+    except FileNotFoundError:
+        sys.exit(f"Could not read {input_file}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+##############################################################################################################################################################################
