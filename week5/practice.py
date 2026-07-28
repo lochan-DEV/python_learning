dc58cf3cc7b98f353bf722909476debd60b17157
lochan-DEV <lochan1410a@gmail.com>
2026-07-28 22:43:32 +0530

    finding biggest number

--- week5/practice.py
+++ week5/practice.py
@@ -1,1 +1,21 @@
 
+def main():
+    print("for example enter the list in this format = 3 4 5 6 34 456 12")
+    x = input("enter list: ")
+    x= x.split()
+    x = [int(p) for p in x]
+    print(find_max(x))
+
+
+def find_max(x):
+    
+    max=x[0]
+    for i in range(len(x)):
+        if x[i]>max:
+            max=x[i]
+        else:
+            pass
+    return max
+
+if __name__ == "__main__":
+    main()
